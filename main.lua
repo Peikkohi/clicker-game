38c1b2cf8867d790be804f99131ca2a5f0c8aded
= <peik.kalliokoski@helsinki.fi>
2023-02-23 11:23:00 +0200

    Cleanup code, remove font depency

--- main.lua
+++ main.lua
@@ -1,97 +1,86 @@
--- variables
-local waves
-
--- objects
+local waves = {}
 local number = {}
 local circle = {}
 
 function love.load(arg)
-  font = love.graphics.newFont('fipps.otf', 32)
-  font:setFilter('nearest')
+	local font = love.graphics.setNewFont(--[['font.ttf',]] 32)
+	font:setFilter('nearest')
 
-  local ww, wh = love.graphics.getDimensions()
-  local file = love.filesystem.newFile('score', 'r')
-  local score
-  if file then
-    score = file:read()
 
-    file:close()
-  end
+	local function get_score()
+		local msg, _ = love.filesystem.read('score')
+		return tonumber(msg)
+	end
 
-  local radius = (ww < wh) and (ww * .3) or (wh * .3)
+	local ww, wh = love.graphics.getDimensions()
+	local radius = (ww < wh) and (ww * .3) or (wh * .3)
 
-  circle.x = ww/2
-  circle.y = wh/2
-  circle.radius = radius
+	-- init objects
+	circle.x = ww / 2
+	circle.y = wh / 2
+	circle.radius = radius
 
-  number.value = tonumber(score) or 0
-  number.x = ww/2
-  number.y = wh/2
-  number.rotation = 0
-  number.scale = 2
-
-  waves = {}
+	number.value = get_score() or 0
+	number.x = ww / 2
+	number.y = wh / 2
+	number.rotation = 0
+	number.scale = 2
 end
 
 function love.update(dt)
-  number.rotation = (number.rotation + dt) % (math.pi*2)
+	number.rotation = (number.rotation + dt) % (math.pi*2)
 
-  for i, e in ipairs(waves) do
-    waves[i] = e - dt * 200
+	for i, e in ipairs(waves) do
+		waves[i] = e - dt * 200
 
-    if e <= 0 then
-      table.remove(waves, i)
-    end
-  end
+		if e <= 0 then
+			table.remove(waves, i)
+		end
+	end
 end
 
 function love.mousepressed(mx, my, bt)
-  if (mx - circle.x)^2 + (my - circle.y)^2 <= circle.radius^2 then
-    number.value = number.value + 1
-    table.insert(waves, circle.radius)
-  end
+	local function sq(x)
+		return x * x
+	end
+	if sq(mx - circle.x) + sq(my - circle.y) <= sq(circle.radius) then
+		number.value = number.value + 1
+		table.insert(waves, circle.radius)
+	end
 end
 
 function love.keypressed(key)
-  if key == 'escape' then love.event.quit() end
+	if key == 'escape' then love.event.quit() end
 end
 
 function love.draw()
-  local ww, wh = love.graphics.getDimensions()
+	love.graphics.circle('line', circle.x, circle.y, circle.radius)
 
-  local rad = ww*.3
+	for i, e in ipairs(waves) do
+		love.graphics.circle('line', circle.x, circle.y, e)
+	end
 
-  love.graphics.circle('line', circle.x, circle.y, circle.radius)
-  -- love.graphics.circle('line', circle.x, circle.y, font:getHeight())
+	local font = love.graphics.getFont()
 
-  for i, e in ipairs(waves) do
-    love.graphics.circle('line', circle.x, circle.y, e)
-  end
-
-  love.graphics.setColor(1,1,1)
-  love.graphics.print(
-    number.value,
-    font,
-    number.x,
-    number.y,
-    number.rotation,
-    number.scale,
-    number.scale,
-    font:getWidth(number.value)/2,
-    font:getHeight()/2
-  )
+	love.graphics.setColor(1,1,1)
+	love.graphics.print(
+		number.value,
+		number.x,
+		number.y,
+		number.rotation,
+		number.scale,
+		number.scale,
+		font:getWidth(number.value)/2,
+		font:getHeight()/2
+	)
 end
 
 function love.visible(visible)
-  if not visible then
-    local file = love.filesystem.newFile('score', 'w')
-    file:write(number.value)
-    file:close()
-  end
+	if not visible then
+		love.filesystem.write('score', number.value)
+	end
 end
 
 function love.quit()
-  local file = love.filesystem.newFile('score', 'w')
-  file:write(number.value)
-  file:close()
+	love.filesystem.write('score', number.value)
 end
